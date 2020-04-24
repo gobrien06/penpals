e69104ae2070cb7f86a03e2bef793b01abe2c0ae
SwampApe <rothvann2000@gmail.com>
2020-04-23 20:57:55 -0700

    Dev config for database

--- backend/database/database.js
+++ backend/database/database.js
@@ -1,15 +1,30 @@
-
+require('dotenv').config({path: '../.env'});
 
 const {Pool, Client} = require('pg');
 
-const client = new Client({
-                        user:
-                        host:
-                        database:
-                        password:
-                        port:
-                        });
+let config = {};
 
+if(process.env.NODE_ENV == 'dev') {
+    config = {
+        user: process.env.TEST_PG_USER,
+        host: 'localhost',
+        database: process.env.TEST_PG_DB,
+        password: process.env.TEST_PG_PASS,
+        port: process.env.TEST_PG_PORT
+    };
+} else {
+     config = {
+        user: process.env.PG_USER,
+        host: 'localhost',
+        database: process.env.PG_DB,
+        password: process.env.PG_PASS,
+        port: process.env.PG_PORT
+    };
+    
+}
+
+
+const client = new Client(config);
 client.connect();
 
 module.exports = client;
